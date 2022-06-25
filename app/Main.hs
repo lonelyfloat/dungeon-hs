762eb72ea7424543604a6fd53cbc7f5c4aa707f0
lonelyfloat <lonelyfloat@pop-os.localdomain>
2022-06-25 15:27:31 -0400

    made better file paths lol

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import DungeonGenerator
+
+main :: IO ()
+main = putStrLn "Hello World"
---
+++ src/DungeonGenerator.hs
@@ -0,0 +1,84 @@
+module DungeonGenerator where
+
+import Data.Time.Clock
+import Data.Maybe
+
+data Room = Room {
+    roomPos :: (Int, Int),
+    roomBounds :: (Int, Int),
+    connectors :: [ConnectorNode],
+    generation :: Int
+} deriving Show
+
+data Direction = North | South | West | East deriving (Eq, Ord, Enum, Show)
+
+data ConnectorNode = ConnectorNode {
+    connectRoom :: Room,
+    connectDir :: Direction,
+    sideOffset :: Int,
+    roomOffset :: Int,
+    connectLength :: Int
+} deriving (Show)
+
+data ConnectorSettings = ConnectorSettings {
+    csDir :: Direction,
+    csOffset :: Int,
+    csrOffset :: Int,
+    csLength :: Int
+} deriving (Show)
+
+data RoomSettings = RoomSettings {
+    connector :: ConnectorNode, 
+    bounds :: (Int, Int),
+    sGen :: Int,
+    willSpawn :: Bool    
+}
+
+data WorldConfig = WorldConfig {
+    xBounds :: (Int, Int),
+    yBounds :: (Int, Int),
+    wBounds :: (Int, Int),
+    hBounds :: (Int, Int),
+    roomSpawnPercent :: Int,
+    connectorSpawnPercent :: Int,
+    totalGenerations :: Int
+}
+
+addConnector :: ConnectorSettings -> Room -> Room
+addConnector (ConnectorSettings dir offset roffset len) room = room {connectors = ConnectorNode room dir offset roffset len : connectors room}
+
+addConnectors :: [ConnectorSettings] -> Room -> Room
+addConnectors settings room = foldr addConnector room settings
+
+roomFromRoomSettings :: RoomSettings -> Room
+roomFromRoomSettings (RoomSettings connect bnds sGen True) = Room {roomPos = getRoomSpawnPos connect, roomBounds = bnds, connectors=[], generation=sGen}
+roomFromRoomSettings (RoomSettings _ _ _ False) = error "Cannot create false room settings"
+
+addRoom :: [Room] -> RoomSettings -> [Room]
+addRoom rooms roomS = if willSpawn roomS then roomFromRoomSettings roomS : rooms else rooms 
+
+getRoomSpawnPos :: ConnectorNode -> (Int, Int)
+getRoomSpawnPos (ConnectorNode room dir offset roffset length)
+    | dir == North = (fst (roomPos room) + offset - roffset, snd (roomPos room) - length)
+    | dir == South = (fst (roomPos room) + offset - roffset, snd (roomPos room) + snd (roomBounds room) + length)
+    | dir == West  = (fst (roomPos room) - length, snd (roomPos room) + offset - roffset)
+    | dir == East  = (fst (roomPos room) + fst (roomBounds room) + length, snd (roomPos room) + offset - roffset)
+    | otherwise = error "Unreachable"
+
+addGeneration :: [Room] -> [[ConnectorSettings]] -> [RoomSettings] -> Int -> [Room]
+addGeneration rooms settings roomSettings currentGeneration = zipWith addConnectors settings (foldl addRoom rooms roomSettings)
+
+-- Random(Generation) Stuff
+
+getRandom :: Int -> Int -> IO Int
+getRandom min max = do
+    t <- getCurrentTime
+    let time = fromIntegral $ diffTimeToPicoseconds $ utctDayTime t
+    return ((((time * 2189104) `div` 2135902) `mod` max) + min)
+
+-- Drawing
+drawRoom :: Room -> IO ()
+drawRoom room = error "Room drawing not implemented"
+
+drawConnector :: ConnectorNode -> IO ()
+drawConnector connect = error "Connector drawing not implemented"
