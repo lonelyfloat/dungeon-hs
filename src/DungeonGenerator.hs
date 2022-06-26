03434e246a39826754ce72c7eb67b82db645750c
lonelyfloat <cephhead_prsnl@protonmail.com>
2022-06-26 00:18:03 -0400

    GIT I SWEAR TO GOD-

--- src/DungeonGenerator.hs
+++ src/DungeonGenerator.hs
@@ -84,7 +84,7 @@ addGeneration :: [Room] -> [[ConnectorSettings]] -> [RoomSettings] -> Int -> [Ro
 addGeneration rooms settings roomSettings currentGeneration = zipWith addConnectors settings (foldl addRoom (filter ((currentGeneration-1==) . generation) rooms) roomSettings) ++
     filter ((currentGeneration-1/=) . generation) rooms 
 
--- Randomsgit config --global user.email you@example.com
+-- THIS COMMENT STOP
 getRandom :: Int -> Int -> IO Int
 getRandom min max = do
     t <- getCurrentTime
